b04bfcf5d368b996f2b26aeffb819e1b87d83164
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2024-10-28 01:01:32 -0300

    Quase finalizado a lógica

--- js/index.js
+++ js/index.js
@@ -1,327 +1,137 @@
+import palavrasAndCategory from "./palavras.js";
+
 const gameStats = {
   playing: false,
-  life: 6,
+  status: 'paused',
+  life: 7,
   secretElement: '',
   palavraSecreta: '',
   categoria: '',
   arrayDePalavras: '',
   wordsClicked: []
-}
+};
 
-function palavrasAndCategory() {
-  return palavras = [
-      palavra001 = {
-          nome: "IRLANDA",
-          categoria:"PAÍS"
-      },
-      palavra002 = {
-          nome: "EQUADOR",
-          categoria:"PAÍS"
-      },
-      palavra003 = {
-          nome: "CHILE",
-          categoria:"PAÍS"
-      },
-      palavra004 = {
-          nome: "INDONESIA",
-          categoria:"PAÍS"
-      },
-      palavra005 = {
-          nome: "MALDIVAS",
-          categoria:"LUGARES"
-      },
-      palavra006 = {
-          nome: "INGLATERRA",
-          categoria:"PAÍS"
-      },
-      palavra007 = {
-          nome: "GROELANDIA",
-          categoria:"PAÍS"
-      },
-      palavra008 = {
-          nome: "UZBEQUISTAO",
-          categoria:"PAÍS"
-      },
-      palavra009 = {
-          nome: "INDONESIA",
-          categoria:"LUGARES"
-      },
-      palavra010 = {
-          nome: "CREGUENHEM",
-          categoria:"LUGARES"
-      },
-      palavra011 = {
-          nome: "BICICLETA",
-          categoria:"TRANSPORTE"
-      },
-      palavra012 = {
-          nome: "LANCHA",
-          categoria:"TRANSPORTE"
-      },
-      palavra013 = {
-          nome: "NAVIO",
-          categoria:"TRANSPORTE"
-      },
-      palavra014 = {
-          nome: "TELEFERICO",
-          categoria:"TRANSPORTE"
-      },
-      palavra015 = {
-          nome: "MOTOCICLETA",
-          categoria:"TRANSPORTE"
-      },
-      palavra016 = {
-          nome: "BARCO",
-          categoria:"TRANSPORTE"
-      },
-      palavra017 = {
-          nome: "AERONAVE",
-          categoria:"TRANSPORTE"
-      },
-      palavra018 = {
-          nome: "TREM",
-          categoria:"TRANSPORTE"
-      },
-      palavra019 = {
-          nome: "CAIAQUE",
-          categoria:"TRANSPORTE"
-      },
-      palavra020 = {
-          nome: "FUNICULAR",
-          categoria:"TRANSPORTE"
-      },
-      palavra021 = {
-          nome: "XICARA",
-          categoria:"OBJETOS"
-      },
-      palavra022 = {
-          nome: "MOEDA",
-          categoria:"OBJETOS"
-      },
-      palavra023 = {
-          nome: "ESPARADRAPO",
-          categoria:"OBJETOS"
-      },
-      palavra024 = {
-          nome: "SINO",
-          categoria:"OBJETOS"
-      },
-      palavra025 = {
-          nome: "CHUVEIRO",
-          categoria:"OBJETOS"
-      },
-      palavra026 = {
-          nome: "TAMBORETE",
-          categoria:"OBJETOS"
-      },
-      palavra027 = {
-          nome: "LAMPADA",
-          categoria:"OBJETOS"
-      },
-      palavra028 = {
-          nome: "BOCAL",
-          categoria:"OBJETOS"
-      },
-      palavra029 = {
-          nome: "CORTINA",
-          categoria:"OBJETOS"
-      },
-      palavra030 = {
-          nome: "LAPIS",
-          categoria:"OBJETOS"
-      },
-      palavra031 = {
-          nome: "MELANCIA",
-          categoria:"ALIMENTOS"
-      },
-      palavra032 = {
-          nome: "AMENDOIM",
-          categoria:"ALIMENTOS"
-      },
-      palavra033 = {
-          nome: "ESFIRRA",
-          categoria:"ALIMENTOS"
-      },
-      palavra034 = {
-          nome: "GOROROBA",
-          categoria:"ALIMENTOS"
-      },
-      palavra035 = {
-          nome: "JANTAR",
-          categoria:"ALIMENTOS"
-      },
-      palavra036 = {
-          nome: "SABOROSO",
-          categoria:"ALIMENTOS"
-      },
-      palavra037 = {
-          nome: "DESJEJUM",
-          categoria:"ALIMENTOS"
-      },
-      palavra038 = {
-          nome: "MASTIGAR",
-          categoria:"ALIMENTOS"
-      },
-      palavra039 = {
-          nome: "ENGOLIR",
-          categoria:"ALIMENTOS"
-      },
-      palavra040 = {
-          nome: "DOCERIA",
-          categoria:"ALIMENTOS"
-      },
-      palavra040 = {
-          nome: "DRAGAO",
-          categoria:"ANIMAIS"
-      },
-      palavra041 = {
-          nome: "GALINHA",
-          categoria:"ANIMAIS"
-      },
-      palavra042 = {
-          nome: "PAVAO",
-          categoria:"ANIMAIS"
-      },
-      palavra043 = {
-          nome: "CAMELO",
-          categoria:"ANIMAIS"
-      },
-      palavra044 = {
-          nome: "PERU",
-          categoria:"ANIMAIS"
-      },
-      palavra045 = {
-          nome: "ZEBRA",
-          categoria:"ANIMAIS"
-      },
-      palavra046 = {
-          nome: "DROMEDARIO",
-          categoria:"ANIMAIS"
-      },
-      palavra047 = {
-          nome: "CALANGO",
-          categoria:"ANIMAIS"
-      },
-      palavra048 = {
-          nome: "SAGUI",
-          categoria:"ANIMAIS"
-      },
-      palavra049 = {
-          nome: "LAGARTIXA",
-          categoria:"ANIMAIS"
-      },
-      palavra050 = {
-          nome: "HIPOPOTAMO",
-          categoria:"ANIMAIS"
-      },
-      palavra051 = {
-          nome: "A ERA DO GELO",
-          categoria:"TV E CINEMA"
-      },
-      palavra052 = {
-          nome: "HOMEM ARANHA",
-          categoria:"TV E CINEMA"
-      },
-      palavra053 = {
-          nome: "CASA MONSTRO",
-          categoria:"TV E CINEMA"
-      },
-      palavra054 = {
-          nome: "TELA QUENTE",
-          categoria:"TV E CINEMA"
-      },
-      palavra055 = {
-          nome: "STRANGER THINGS",
-          categoria:"TV E CINEMA"
-      },
-      palavra056 = {
-          nome: "O REI DO GADO",
-          categoria:"TV E CINEMA"
-      },
-      palavra057 = {
-          nome: "MULHER MARAVILHA",
-          categoria:"TV E CINEMA"
-      },
-      palavra058 = {
-          nome: "O INCRIVEL HULK",
-          categoria:"TV E CINEMA"
-      },
-      palavra059 = {
-          nome: "BOB ESPONJA",
-          categoria:"TV E CINEMA"
-      },
-      palavra060 = {
-          nome: "PANICO NA TV",
-          categoria:"TV E CINEMA"
-      }
-  ];
-}
+const keysActivs = [
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+];
 
 function gerarPalavraSecreta(arr) {
-  const i = Math.floor(Math.random() * arr.length)
-  return gameStats.secretElement = arr[i]
+  const i = Math.floor(Math.random() * arr.length);
+  return gameStats.secretElement = arr[i];
 }
 
 function createInput(palavra) {
-  const content = document.querySelector('.content-word')
+  const content = document.querySelector('.content-word');
+  content.innerHTML = ''; 
 
   for(let i = 0; i < palavra.length; i++) {
+    const box = document.createElement('span');
+    box.dataset.position = i;
     if (palavra[i] === ' ') {
-      const space = document.createElement('span')
-      space.classList.add('box')
-      space.textContent = ' '
-      content.append(space)
+        box.classList.add('box', 'space');
+        content.append(box)
     } else {
-      const box = document.createElement('span')
-      box.classList.add('box', 'letra')
-      box.dataset.position = i
-      box.textContent = palavra[i]
-      content.append(box)
+        box.classList.add('box', 'letra');
+        box.textContent = '_'
+        content.append(box);
     }
   }
 }
 
 function startGame() {
-  gameStats.life = 6
-  gameStats.arrayDePalavras = palavrasAndCategory()
-  gameStats.secretElement = gerarPalavraSecreta(gameStats.arrayDePalavras)
-  gameStats.palavraSecreta = gameStats.secretElement.nome
-  gameStats.categoria = gameStats.secretElement.categoria
+  gameStats.life = 7;
+  gameStats.playing = true;
+  gameStats.status = 'playing';
+  gameStats.wordsClicked = [];
+  gameStats.arrayDePalavras = palavrasAndCategory();
+  gameStats.secretElement = gerarPalavraSecreta(gameStats.arrayDePalavras);
+  gameStats.palavraSecreta = gameStats.secretElement.nome.toLowerCase();
+  gameStats.categoria = gameStats.secretElement.categoria;
+
+  document.getElementById('category').innerText = `Categoria: ${gameStats.categoria}`;
+  document.getElementById('tenttive').innerText = `Tentativas restantes: ${gameStats.life}`;
 
   console.log(gameStats.palavraSecreta);
-  console.log(gameStats.categoria);
-  
-  createInput(gameStats.palavraSecreta)
+  createInput(gameStats.palavraSecreta);
 }
 
-function playerLife() {
-  const img = document.getElementById('forca-image')
-  switch (gameStats.life) {
-    case 6:
-      img.src = '../img/forca.png'
-      break;
-  
-    default:
-      break;
+function updatePlayerLife(life) {
+  const img = document.getElementById('forca-image');
+  img.src = `../img/forca0${7 - life}.png`;
+
+  if (life <= 0) {
+    img.src = '../img/forca06.png'
+    gameStats.status = 'game-over';
+
+    const revelate = document.querySelector('.text')
+    revelate.classList.remove('display')
+    const palavra = document.querySelector('.revelate-palavra')
+    palavra.textContent = `"${gameStats.palavraSecreta.toUpperCase()}"`
   }
 }
 
-const starGameBtn = document.getElementById('play-game')
+const starGameBtn = document.getElementById('play-game');
 starGameBtn.addEventListener('click', () => {
-  const header = document.querySelector('header')
-  header.classList.add('display')
+  document.querySelector('header').classList.add('display');
+  document.querySelector('.content').classList.remove('display');
+  
+  startGame();
+  keyBoardFn();
+});
 
-  const container = document.querySelector('.content')
-  container.classList.remove('display')
+function disableKey(key, palavra) {
+  if (gameStats.status === 'playing' && !gameStats.wordsClicked.includes(key)) {
+    gameStats.wordsClicked.push(key);
 
-  gameStats.playing = true
+    if (!palavra.includes(key)) {
+      gameStats.life--
+      document.getElementById('tenttive').innerText = `Tentativas restantes: ${gameStats.life}`;
+      updatePlayerLife(gameStats.life);
+    } else {
+      addLetra(key, palavra);
+    }
 
-  const category = document.getElementById('category')
-  category.innerText = `Categoria: ${gameStats.categoria}`
-
-  playerLife()
-})
-
-if (gameStats) {
-  startGame()
+    if (checkIfWinner()) {
+      gameStats.status = 'winner';
+      alert('Parabéns, você venceu!');
+    }
+  }
 }
+
+function keyBoardFn() {
+  document.addEventListener('keypress', (ev) => {
+    if (gameStats.status === 'playing') {
+      const key = ev.key.toLowerCase();
+      if (keysActivs.includes(key)) {
+        disableKey(key, gameStats.palavraSecreta);
+      }
+    }
+  });
+}
+
+function addLetra(key, palavra) {
+  palavra.split("").forEach((l, i) => {
+    if (l === key) {
+      changeBoxInside(l, i);
+    }
+  });
+}
+
+function changeBoxInside(key, index) {
+  document.querySelectorAll('.letra').forEach((element) => {
+    if (parseInt(element.dataset.position) === index) {
+      element.textContent = key;
+    }
+  });
+}
+
+function checkIfWinner() {
+    const checkPalavra = [...document.querySelectorAll('.letra')]
+      .map(el => el.textContent)
+      .join('')
+      .replace(/\s+/g, ''); 
+  
+    return checkPalavra === gameStats.palavraSecreta.replace(/\s+/g, '');
+  }
+  
